69d6c4c0865f1634a47893eb6e9d62646a779a29
usr110 <m.aliabbas@gmail.com>
2019-10-30 15:41:41 +0000

    Adjust 15-19 to 16-19 age group

--- code/script.R
+++ code/script.R
@@ -3,3 +3,13 @@ require(tidyverse)
 
 # read data
 rd <- read_csv('data/IHME-GBD_2017_DATA-7f79a465-1.csv')
+
+## Adjust val, upper and lower values for 16-19 age group
+
+# Reduce the value by 20%
+rd[rd$age_name == "15 to 19",]$val <- rd[rd$age_name == "15 to 19",]$val * 0.8
+rd[rd$age_name == "15 to 19",]$upper <- rd[rd$age_name == "15 to 19",]$lower * 0.8
+rd[rd$age_name == "15 to 19",]$lower <- rd[rd$age_name == "15 to 19",]$lower * 0.8
+
+# Rename the age group
+rd[rd$age_name == "15 to 19",]$age_name <- "16 to 19"
